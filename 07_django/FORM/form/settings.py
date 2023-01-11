f10ee5e9859683cef184ba880a7e3b31380291bb
Datastation7 <datastation7@gmail.com>
2023-01-11 18:20:02 +0900

    django

--- 07_django/FORM/form/settings.py
+++ 07_django/FORM/form/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3fuaj62$@jqim8%!%_#zv#7n_)im*h*)ce9j2be^3)(6#2#krr'
+SECRET_KEY = 'django-insecure-wt9$@xcl_as)zxd@np62jish9+$#a068$5)3(mwoglax^8m^!%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # native apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'django_extensions',
+    'bootstrap5',
+    # local apps
+    'classroom',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'form.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
